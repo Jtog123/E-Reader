3b95110e5c898721843be870668d9720004234f1
jttest github <jamestogher567@gmail.com>
2023-08-05 09:39:57 -0700

    variables renaming

--- eReader.py
+++ eReader.py
@@ -22,52 +22,52 @@ class MainWindow(QMainWindow):
         width = 800
         height = 800
         self.setMinimumSize(width, height)
-        self.initUI()
+        self.init_ui()
         self.set_styles()
 
-    def initUI(self):
+    def init_ui(self):
 
         #Opens file through using Tab, also includes shortcut for opening a file
         self.button_action = QAction(QIcon('icons/folder-horizontal-open.png'), 'Open File', self)
-        self.button_action.triggered.connect(self.readFile)
+        self.button_action.triggered.connect(self.read_file)
         self.button_action.setShortcut( QKeySequence("Ctrl+o") )
 
 
         #Create menu bar at top of screen
         self.menu = self.menuBar()
-        fileMenu = self.menu.addMenu('&File')
-        fileMenu.addAction(self.button_action)
+        file_menu = self.menu.addMenu('&File')
+        file_menu.addAction(self.button_action)
 
         #Create view tab?
 
         #create button that opens file dialog
-        self.openFilebutton = QPushButton("&Open File", self)
-        self.openFilebutton.clicked.connect(self.readFile)
+        self.open_file_button = QPushButton("&Open File", self)
+        self.open_file_button.clicked.connect(self.read_file)
         
 
 
         #Dictionary Button toggle back and forth between reader
-        self.dictButton = QPushButton("&Dictionary", self)
-        self.dictButton.clicked.connect(self.dictionary_page)
+        self.dict_button = QPushButton("&Dictionary", self)
+        self.dict_button.clicked.connect(self.dictionary_page)
         
 
         #Reader Button toggle back and forth between dictionary
-        self.readerButton = QPushButton("&Reader", self)
-        self.readerButton.setEnabled(False) #set to true when we click on dictionary button
-        self.readerButton.clicked.connect(self.reader_page)
+        self.reader_button = QPushButton("&Reader", self)
+        self.reader_button.setEnabled(False) #set to true when we click on dictionary button
+        self.reader_button.clicked.connect(self.reader_page)
 
-        self.searchButton = QPushButton("&Search", self)
-        self.searchButton.clicked.connect(self.search_dict)
+        self.search_button = QPushButton("&Search", self)
+        self.search_button.clicked.connect(self.search_dict)
 
         #Search word label
         self.search_word_label = QLabel("")
 
         #Dictionary label
-        self.dictLabel = QLabel("Your search result will appear here.")
-        self.dictLabel.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-        self.dictLabel.setWordWrap(True)
-        self.dictFont = QFont("Arial", 12, QFont.Bold)
-        self.dictLabel.setFont(self.dictFont)
+        self.dict_label = QLabel("Your search result will appear here.")
+        self.dict_label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        self.dict_label.setWordWrap(True)
+        self.dict_font = QFont("Arial", 12, QFont.Bold)
+        self.dict_label.setFont(self.dict_font)
 
 
         #Tab Widget
@@ -83,62 +83,62 @@ class MainWindow(QMainWindow):
 
 
         #Corner tab button
-        self.tabPlus = QToolButton(self)
-        self.tabPlus.setText('+')
-        self.tabPlus.setStyleSheet("background-color:#c484a3")
-        self.tabs.setCornerWidget(self.tabPlus)
-        self.tabPlus.clicked.connect(self.readFile)
+        self.tab_plus = QToolButton(self)
+        self.tab_plus.setText('+')
+        self.tab_plus.setStyleSheet("background-color:#c484a3")
+        self.tabs.setCornerWidget(self.tab_plus)
+        self.tab_plus.clicked.connect(self.read_file)
 
         #Search Bar
-        self.searchBar = QLineEdit()
-        self.searchBar.setText("Search")
+        self.search_bar = QLineEdit()
+        self.search_bar.setText("Search")
 
         #Create the stackedwidget and the mainlayout which is a vertical box layout
-        self.pageStack = QStackedWidget()
-        mainLayout = QVBoxLayout()
+        self.page_stack = QStackedWidget()
+        main_layout = QVBoxLayout()
 
 
         # Create a central widget, set the layout as the main layout
         central_widget = QWidget()
-        central_widget.setLayout(mainLayout)
+        central_widget.setLayout(main_layout)
 
         # Make the windows central widget the central_widget variable
         self.setCentralWidget(central_widget)
 
         #Search Header Widget
-        self.headerWidget = QWidget()
-        searchLayout = QHBoxLayout(self.headerWidget)
-        searchLayout.addWidget(self.searchBar)
-        searchLayout.addWidget(self.searchButton)
-        searchLayout.addStretch()
-        mainLayout.addWidget(self.headerWidget)
-        self.headerWidget.hide()
+        self.header_widget = QWidget()
+        search_layout = QHBoxLayout(self.header_widget)
+        search_layout.addWidget(self.search_bar)
+        search_layout.addWidget(self.search_button)
+        search_layout.addStretch()
+        main_layout.addWidget(self.header_widget)
+        self.header_widget.hide()
         
 
         #Add the stackedWidget to the mainlayout, add the tabswidget to the stackedwidget
-        mainLayout.addWidget(self.pageStack)
-        self.pageStack.addWidget(self.tabs)
+        main_layout.addWidget(self.page_stack)
+        self.page_stack.addWidget(self.tabs)
 
 
         #Footer Layout with buttons added
-        footerWidget = QWidget()
-        hLay = QHBoxLayout(footerWidget)
-        hLay.addWidget(self.openFilebutton, alignment=Qt.AlignLeft)
-        hLay.addStretch()
-        hLay.addWidget(self.readerButton, alignment=Qt.AlignRight)
-        hLay.addWidget(self.dictButton,alignment=Qt.AlignRight)
+        footer_widget = QWidget()
+        h_lay = QHBoxLayout(footer_widget)
+        h_lay.addWidget(self.open_file_button, alignment=Qt.AlignLeft)
+        h_lay.addStretch()
+        h_lay.addWidget(self.reader_button, alignment=Qt.AlignRight)
+        h_lay.addWidget(self.dict_button,alignment=Qt.AlignRight)
 
         
         #Add the footer widget to the main layout
-        mainLayout.addWidget(footerWidget)
+        main_layout.addWidget(footer_widget)
 
 
     #Read files
-    def readFile(self):
-        fname = QFileDialog.getOpenFileName(self, "Open File", "c:\\", "PDF Files (*.pdf)")
-        fnameString = str(fname[0])
-        if fnameString != '':
-            self.add_new_tab(fnameString)
+    def read_file(self):
+        f_name = QFileDialog.getOpenFileName(self, "Open File", "c:\\", "PDF Files (*.pdf)")
+        f_name_string = str(f_name[0])
+        if f_name_string != '':
+            self.add_new_tab(f_name_string)
             
     #Add Tabs
     def add_new_tab(self, fname):
@@ -156,54 +156,54 @@ class MainWindow(QMainWindow):
             view.show()
 
     def insert_page(self, index = -1):
-        self.pageStack.insertWidget(index, self.dictLabel) #Might have to create own label
+        self.page_stack.insertWidget(index, self.dict_label) #Might have to create own label
 
         
     def dictionary_page(self):
-        self.headerWidget.show()
+        self.header_widget.show()
 
-        new_index = self.pageStack.currentIndex() + 1
-        if new_index < len(self.pageStack):
-            self.pageStack.setCurrentIndex(new_index)  
+        new_index = self.page_stack.currentIndex() + 1
+        if new_index < len(self.page_stack):
+            self.page_stack.setCurrentIndex(new_index)  
 
-        self.readerButton.setEnabled(True)
-        self.dictButton.setEnabled(False)
-        self.openFilebutton.setEnabled(False)
+        self.reader_button.setEnabled(True)
+        self.dict_button.setEnabled(False)
+        self.open_file_button.setEnabled(False)
 
 
     def reader_page(self):
-        self.headerWidget.hide()
+        self.header_widget.hide()
 
-        new_index = self.pageStack.currentIndex() - 1
+        new_index = self.page_stack.currentIndex() - 1
         if new_index >= 0:
-            self.pageStack.setCurrentIndex(new_index)
+            self.page_stack.setCurrentIndex(new_index)
         
-        self.readerButton.setEnabled(False)
-        self.dictButton.setEnabled(True)
-        self.openFilebutton.setEnabled(True)
+        self.reader_button.setEnabled(False)
+        self.dict_button.setEnabled(True)
+        self.open_file_button.setEnabled(True)
     
     def search_dict(self):
         dictionary = Dictionary() #Create dict
-        searchText = self.searchBar.text() #get text from bar
+        search_text = self.search_bar.text() #get text from bar
 
-        if searchText != '':
-            self.dictLabel.setStyleSheet("color: black")
-            self.dictLabel.setStyleSheet("background-color: #dcdedc;")
-            dictionary.update_url(searchText) #Update url with new search term
-            self.dictLabel.setText(searchText +": "+ "\n" + dictionary.recievedDefinition)
+        if search_text != '':
+            self.dict_label.setStyleSheet("color: black")
+            self.dict_label.setStyleSheet("background-color: #dcdedc;")
+            dictionary.update_url(search_text) #Update url with new search term
+            self.dict_label.setText(search_text +": "+ "\n" + dictionary.recieved_definition)
         else:
-            self.dictLabel.setStyleSheet("color: red")
-            self.dictLabel.setText("Enter a valid word")
+            self.dict_label.setStyleSheet("color: red")
+            self.dict_label.setText("Enter a valid word")
     
     def set_styles(self):
         self.setStyleSheet("background-color: #a2aba5;")
         self.menu.setStyleSheet("background-color: #c2c4c3;")
-        self.openFilebutton.setStyleSheet("background-color: #c2c4c3;")
-        self.readerButton.setStyleSheet("background-color: #c2c4c3;")
-        self.dictButton.setStyleSheet("background-color: #c2c4c3;")
-        self.searchButton.setStyleSheet("background-color: #c2c4c3;")
-        self.searchBar.setStyleSheet("background-color: #c2c4c3;")
-        self.dictLabel.setStyleSheet("background-color: #dcdedc;")
+        self.open_file_button.setStyleSheet("background-color: #c2c4c3;")
+        self.reader_button.setStyleSheet("background-color: #c2c4c3;")
+        self.dict_button.setStyleSheet("background-color: #c2c4c3;")
+        self.search_button.setStyleSheet("background-color: #c2c4c3;")
+        self.search_bar.setStyleSheet("background-color: #c2c4c3;")
+        self.dict_label.setStyleSheet("background-color: #dcdedc;")
 
 
     #adjust names to be pythonic
@@ -217,11 +217,11 @@ class Dictionary():
         self.app_id = os.environ.get("READER_ID") #YOUR API ID HERE
         self.app_key = os.environ.get("READER_API_KEY") # YOUR API KEY HERE
         self.language = "en-gb"
-        self.searchWord = "search"
-        self.url = "https://od-api.oxforddictionaries.com:443/api/v2/words/" + self.language + "?q=" + self.searchWord + "&fields=definitions"  
+        self.search_word = "search"
+        self.url = "https://od-api.oxforddictionaries.com:443/api/v2/words/" + self.language + "?q=" + self.search_word + "&fields=definitions"  
         self.request = requests.get(self.url, headers={"app_id": self.app_id, "app_key": self.app_key})
         self.data = self.request.json()
-        self.recievedDefinition = self.data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+        self.recieved_definition = self.data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
     
     def update_url(self, word):
         self.url = "https://od-api.oxforddictionaries.com:443/api/v2/words/" + self.language + "?q=" + word + "&fields=definitions" 
@@ -232,9 +232,9 @@ class Dictionary():
         try:
             self.request = requests.get(self.url, headers={"app_id": self.app_id, "app_key": self.app_key})
             self.data = self.request.json()
-            self.recievedDefinition = self.data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+            self.recieved_definition = self.data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
         except KeyError:
-            self.recievedDefinition = "EXCEPTION THROWN: Potentially misspelled word, or word doesnt exist in dictionary." #Rephrase this
+            self.recieved_definition = "EXCEPTION THROWN: Potentially misspelled word, or word doesnt exist in dictionary." #Rephrase this
 
         
 if __name__ == '__main__':
